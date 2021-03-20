fdd0e87bf03270cd2d38c83481b0669eb50a9df4
utpanwar <utpanwar75@gmail.com>
2021-03-20 11:14:08 +0530

    chane in IcaSer

--- Services/ICharaterService.cs
+++ Services/ICharaterService.cs
@@ -1,15 +1,16 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Controllers.Models;
+using Dtos.Character;
 using Models;
 
 namespace Services
 {
     public interface ICharaterService
     {
-         Task<ServiceResponse<List<Character>>> GetAllCharacters();
-         Task<ServiceResponse<Character>> GetCharacterById(int id);
-         Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
+         Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
+         Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
 
     }
 }
